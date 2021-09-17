10867a35abf5d6169db02de6ae0b39d56647aa3e
ping58972 <ping58972@gmail.com>
2021-09-17 16:36:17 -0500

    update on sep 17 2021

--- Parallel_and_Distributed_Algorithms-ICS440_01-Fall2021/Eclipse-workspace/ICS440-HW02-Fruit/src/java/com/abc/fruit/FruitDemo.java
+++ Parallel_and_Distributed_Algorithms-ICS440_01-Fall2021/Eclipse-workspace/ICS440-HW02-Fruit/src/java/com/abc/fruit/FruitDemo.java
@@ -12,6 +12,7 @@ public class FruitDemo {
     /**
      * @param args
      */
+    @SuppressWarnings("unused")
     public static void main(String[] args) throws InterruptedException {
         new Fruit("Apple");
         new Fruit("Banana");
