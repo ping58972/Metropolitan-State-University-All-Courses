0045139d2754f086a19e0ff497ee44f238fb3c31
ping58972 <ping58972@gmail.com>
2021-10-15 16:43:51 -0500

    update on Oct 15

---
+++ Parallel_and_Distributed_Algorithms-ICS440_01-Fall2021/Eclipse-workspace/s07_practice/src/java/com/abc/main/StringBox.java
@@ -0,0 +1,23 @@
+package com.abc.main;
+
+/**
+ *
+ */
+public class StringBox {
+
+    private String content;
+
+    public synchronized void put(String newContent) {
+
+    }
+    public synchronized String getContent() {
+        return content;
+    }
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
