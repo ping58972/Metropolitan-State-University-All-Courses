da06ffa154c0455c59891e4c6b7a322c0933eda9
ping58972 <ping58972@gmail.com>
2021-10-23 16:49:56 -0500

    update Oct 23

--- Parallel_and_Distributed_Algorithms-ICS440_01-Fall2021/Eclipse-workspace/s07_practice/src/java/com/abc/main/StringBox.java
+++ Parallel_and_Distributed_Algorithms-ICS440_01-Fall2021/Eclipse-workspace/s07_practice/src/java/com/abc/main/StringBox.java
@@ -1,57 +1,82 @@
 package com.abc.main;
 
+import java.util.*;
+
 /**
  *
  */
 public class StringBox {
 
-    private String content;
+    // private String content;
+    //
+    // public synchronized void put(String newContent) {
+    //
+    // }
+    //
+    // public synchronized String getContent() {
+    // return content;
+    // }
 
-    public synchronized void put(String newContent) {
-
-    }
-    public synchronized String getContent() {
-        return content;
-    }
     /**
      * @param args
      */
     public static void main(String[] args) {
-
+        System.out.println((int) '9');// 57
+        // String strNum = "21462675756";
+        String strNum = "98676555533";
+        String result = "";
+        Deque<Character> q = new LinkedList<>();
+        for (char c : strNum.toCharArray()) {
+            if (q.isEmpty())
+                q.add(c);
+            else if ( ((int) q.peekLast() % 2 == 0) && (c % 2 == 0)) {
+                q.add('*');
+                q.add(c);
+            } else if ( ((int) q.peekLast() % 2 != 0) && (c % 2 != 0)) {
+                q.add('-');
+                q.add(c);
+            } else {
+                q.add(c);
+            }
+        }
+        while ( !q.isEmpty())
+            result += q.poll();
+        System.out.println(result);
     }
 
-//    public static int[] solution (int[] A, int F, int M) {
-//        int[] result = new int[F];
-//        int number_of_rolls = A.length + F;
-//        int sum_of_rolls = M * number_of_rolls;
-//        int sum_of_known_rolls = 0;
-//
-//            for(int roll_val: A)
-//                sum_of_known_rolls += roll_val;
-//            int sum_of_unknown_rolls = sum_of_rolls - sum_of_known_rolls;
-//            //if the sum_of_unknown_rolls is out of range:
-//            if(sum_of_unknown_rolls < F || sum_of_unknown_rolls > 6 * F)
-//                return new int[1]; //possible rolls doesn't exist
-//            /*all is left is to return one of the possible solutions. we will "fill" the dice results
-//             * with the maximum values possible (6 if possible), and when we hit the "lower bound" (meaning the
-//             * sum_of_unkown_rolls left is equal to the number of rolls left) it means the rest of the
-//             * dice rolls are "1".*/
-//            for (int i = 0; i < result.length; i++)
-//            {
-//                if (sum_of_unknown_rolls - (F - 1) <= 6)
-//                {
-//                    result[i] = sum_of_unknown_rolls - (F - 1);
-//                    Arrays.fill(result, i + 1, result.length, 1);
-//                    break;
-//                }
-//                else
-//                {
-//                    result[i] = 6;
-//                    sum_of_unknown_rolls -= 6;
-//                    F--;
-//                }
-//            }
-//            return result;
-//    }
+    // public static int[] solution (int[] A, int F, int M) {
+    // int[] result = new int[F];
+    // int number_of_rolls = A.length + F;
+    // int sum_of_rolls = M * number_of_rolls;
+    // int sum_of_known_rolls = 0;
+    //
+    // for(int roll_val: A)
+    // sum_of_known_rolls += roll_val;
+    // int sum_of_unknown_rolls = sum_of_rolls - sum_of_known_rolls;
+    // //if the sum_of_unknown_rolls is out of range:
+    // if(sum_of_unknown_rolls < F || sum_of_unknown_rolls > 6 * F)
+    // return new int[1]; //possible rolls doesn't exist
+    // /*all is left is to return one of the possible solutions. we will "fill" the dice results
+    // * with the maximum values possible (6 if possible), and when we hit the "lower bound"
+    // (meaning the
+    // * sum_of_unkown_rolls left is equal to the number of rolls left) it means the rest of the
+    // * dice rolls are "1".*/
+    // for (int i = 0; i < result.length; i++)
+    // {
+    // if (sum_of_unknown_rolls - (F - 1) <= 6)
+    // {
+    // result[i] = sum_of_unknown_rolls - (F - 1);
+    // Arrays.fill(result, i + 1, result.length, 1);
+    // break;
+    // }
+    // else
+    // {
+    // result[i] = 6;
+    // sum_of_unknown_rolls -= 6;
+    // F--;
+    // }
+    // }
+    // return result;
+    // }
 
 }
