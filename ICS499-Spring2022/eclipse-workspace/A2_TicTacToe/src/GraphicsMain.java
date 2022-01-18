dcf40bec9a9f3bec64ace4fddc20706f87931ea3
ping58972 <ping58972@gmail.com>
2022-01-18 15:14:41 -0600

    update Jan 18, 2022

--- ICS499-Spring2022/eclipse-workspace/A2_TicTacToe/src/GraphicsMain.java
+++ ICS499-Spring2022/eclipse-workspace/A2_TicTacToe/src/GraphicsMain.java
@@ -1,51 +1,127 @@
 
 import processing.core.PApplet;
 
+/*
+ * Edit and Update By Nalongsone Danddank
+ * ICS499-01
+ * Student ID: 14958950
+ * email: nalongsone.danddank@my.metrostate.edu
+ * */
 /**
- * The GraphicsMain class will extend PApplet.
- *  It should have the methods main(), settings(), setup() and draw(). 
+ * The GraphicsMain class will extend PApplet. It should have the methods
+ * main(), settings(), setup() and draw().
  * 
- *  In setup(), you should call the TicTacToe constructor. 
- *  In the draw() method, you should display the board and handle the events/clicks
- *  
+ * In setup(), you should call the TicTacToe constructor. In the draw() method,
+ * you should display the board and handle the events/clicks
+ * 
  * 
  */
 public class GraphicsMain extends PApplet {
-	
+
 	// declare any variables you need
-	 
-	
+	TicTacToe ticTacToe;
+	Box[] boxes = new Box[9];
+	int clickControls[] = new int[9];
 
 	public static void main(String[] args) {
-
 		PApplet.main("GraphicsMain");
-
 	}
 
-	public void settings()
-	{
-		size(800, 800);
+	public void settings() {
+		size(500, 500);
 	}
 
-	public void setup()
-	{
+	public void setup() {
 		// TODO: create the tic tac toe board.
-		// Use the rect() to draw the board
-		
-		background(255); 
-		fill(255, 0, 0, 30);
+		// create and initilize every box by fixed size.
+		for (int i = 0; i < 9; i++) {
+			boxes[i] = new Box(100 + 100 * (i % 3), 100 + 100 * (i / 3));
+		}
+		// Create new game (it will intialize to empty board)
+		ticTacToe = new TicTacToe();
+		textSize(26);
+		fill(50);
+		text("Well Come To TIC-TAC-TOE", 100, 50);
 	}
 
 	public void draw() {
-	
-		// TODO:  handle the events
-		// based on where the user clicked, update the display
-		// place X or O on the board
-		
+		// TODO: handle the events
+		background(200);
+		textSize(26);
+		fill(50);
+		text("Well Come To TIC-TAC-TOE", 100, 50);
+		text("Mr.X  Vs.  Ms.O", 180, 80);
+		for (int i = 0; i < 9; i++) {
+			// build, update and display every box.
+			boxes[i].drawBox(mouseX, mouseY);
+			boxes[i].updateBox(i);
+		}
 		// Keep checking for the winner to display a message
-		// X or O or draw 
+		// X or O or draw
+		// if the game get winner or done all box.
+		// stop the click event and print the result.
+		String checking = ticTacToe.checkWinner();
+		if (!checking.equals("None")) {
+			for (int j = 0; j < 9; j++) {
+				clickControls[j] = 10000;
+			}
+			if (checking.equals("draw")) {
+				text("Game Over!!!", 100, 450);
+			} else if (checking.equals("X")) {
+				text("Mr.X, You Win!", 100, 450);
+			} else if (checking.equals("O")) {
+				text("Mr.O, You Win!", 100, 450);
+			}
+		} else {
+			text(ticTacToe.getTurn(), mouseX, mouseY);
+		}
 	}
 
-	
+	// based on where the user clicked, update the display
+	// place X or O on the board
+	public void mouseClicked() {
+		// click each mouse clicked position to handle the event and update the Board
+		// then turn user to another.
+		for (int i = 0; i < 9; i++) {
+			if ((mouseX > clickControls[i] + 100 + (100 * (i % 3)) && mouseX < clickControls[i] + 200 + (100 * (i % 3)))
+					&& (mouseY > clickControls[i] + 100 + (100 * (i / 3))
+							&& mouseY < clickControls[i] + 200 + (100 * (i / 3)))) {
+				ticTacToe.getBoard()[i] = ticTacToe.getTurn();
+				if (ticTacToe.getTurn().equals("X")) {
+					ticTacToe.setTurn("O");
+				} else {
+					ticTacToe.setTurn("X");
+				}
+				clickControls[i] = -10000;
+			}
+		}
 
+	}
+
+	private class Box {
+		/**
+		 * Box for display each number and O or X .
+		 */
+		int rectX, rectY; // Position of square Box
+		int rectSize = 100;
+		int rectHighlight;
+
+		public Box(int X, int Y) {
+			rectHighlight = color(251);
+			rectX = X;
+			rectY = Y;
+		}
+
+		public void drawBox(int mouseXBox, int mouseYBox) {
+			fill(rectHighlight);
+			rect(rectX, rectY, rectSize, rectSize);
+		}
+
+		public void updateBox(int i) {
+			fill(0);
+			textSize(60);
+			String str = ticTacToe.getBoard()[i];
+			text(str, rectX + 45, rectY + 60);
+		}
+	}
 }
--- ICS499-Spring2022/eclipse-workspace/A2_TicTacToe/src/TicTacToe.java
+++ ICS499-Spring2022/eclipse-workspace/A2_TicTacToe/src/TicTacToe.java
@@ -1,186 +1,164 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
+public class TicTacToe {
 
+	// for holding the players turn
+	private String turn;
 
-public class TicTacToe
-{	
+	// for representing the board
+	private String[] board;
 
-	//for holding the players turn
-	private String turn;	
-
-	//for representing the board
-	private String[ ] board;	
-
-	//for holding the winner information (X or O or draw or none)
+	// for holding the winner information (X or O or draw or none)
 	private String winner;
 
 	/**
 	 * Default Constructor For TicTacToe Class
 	 */
-	 public TicTacToe()
-	 {		 
-		 this.setTurn("X");
-		 this.setWinner("None");
-		
-		 String[] board = new String[9];
-		 for (int a = 0; a < 9; a++) {
+	public TicTacToe() {
+		this.setTurn("X");
+		this.setWinner("None");
+
+		String[] board = new String[9];
+		for (int a = 0; a < 9; a++) {
 			board[a] = String.valueOf(a + 1);
-		 }
+		}
 		this.setBoard(board);
- 
-	 };
+
+	};
 
 	/**
-	* Overloaded Constructor For TicTacToe Class
-	*/
-	 public TicTacToe(String a_turn, String[ ] a_board, String a_winner)
-	 { 
+	 * Overloaded Constructor For TicTacToe Class
+	 */
+	public TicTacToe(String a_turn, String[] a_board, String a_winner) {
 		turn = a_turn;
 		board = a_board;
 		winner = a_winner;
-	 }
+	}
 
 	/**
 	 * Set method for the variable turn
 	 */
-	public void setTurn(String a_turn)
-	{
+	public void setTurn(String a_turn) {
 		turn = a_turn;
 	}
 
 	/**
 	 * Set method for the variable board
 	 */
-	public void setBoard(String[ ] a_board)
-	{
+	public void setBoard(String[] a_board) {
 		board = a_board;
 	}
 
-	
 	/**
 	 * Set method for the variable winner
 	 */
-	public void setWinner(String a_winner)
-	{
+	public void setWinner(String a_winner) {
 		winner = a_winner;
 	}
 
 	/**
 	 * Get method for the variable turn
 	 */
-	public String getTurn( )
-	{
+	public String getTurn() {
 		return turn;
 	}
 
 	/**
 	 * Get method for the variable board
 	 */
-	public String[ ] getBoard( )
-	{
+	public String[] getBoard() {
 		return board;
 	}
-
 
 	/**
 	 * Get method for the variable winner
 	 */
-	public String getWinner( )
-	{
+	public String getWinner() {
 		return winner;
 	}
 
-	
-	 
-	
-	 /**
-	  * method for getting the winner
-	  * return value can be X or O or 'draw' or 'null'
-	  * @return
-	  */
-		public String checkWinner()
-		{
-			for (int a = 0; a < 8; a++) {
-				String line = null;
+	/**
+	 * method for getting the winner return value can be X or O or 'draw' or 'null'
+	 * 
+	 * @return
+	 */
+	public String checkWinner() {
+		for (int a = 0; a < 8; a++) {
+			String line = null;
 
-				switch (a) {
-				case 0:
-					line = board[0] + board[1] + board[2];
-					break;
-				case 1:
-					line = board[3] + board[4] + board[5];
-					break;
-				case 2:
-					line = board[6] + board[7] + board[8];
-					break;
-				case 3:
-					line = board[0] + board[3] + board[6];
-					break;
-				case 4:
-					line = board[1] + board[4] + board[7];
-					break;
-				case 5:
-					line = board[2] + board[5] + board[8];
-					break;
-				case 6:
-					line = board[0] + board[4] + board[8];
-					break;
-				case 7:
-					line = board[2] + board[4] + board[6];
-					break;
-				}
-				//For X winner
-				if (line.equals("XXX")) {
-					return "X";
-				}
-				
-				// For O winner
-				else if (line.equals("OOO")) {
-					return "O";
-				}
+			switch (a) {
+			case 0:
+				line = board[0] + board[1] + board[2];
+				break;
+			case 1:
+				line = board[3] + board[4] + board[5];
+				break;
+			case 2:
+				line = board[6] + board[7] + board[8];
+				break;
+			case 3:
+				line = board[0] + board[3] + board[6];
+				break;
+			case 4:
+				line = board[1] + board[4] + board[7];
+				break;
+			case 5:
+				line = board[2] + board[5] + board[8];
+				break;
+			case 6:
+				line = board[0] + board[4] + board[8];
+				break;
+			case 7:
+				line = board[2] + board[4] + board[6];
+				break;
 			}
-			
-			// check if all the slots are full and if there is no winner
-			// return draw
-			for (int a = 0; a < 9; a++) {
-				if (Arrays.asList(board).contains(String.valueOf(a + 1))) {
-					break;
-				}
-				else if (a == 8) {
-					return "draw";
-				}
+			// For X winner
+			if (line.equals("XXX")) {
+				return "X";
 			}
+
+			// For O winner
+			else if (line.equals("OOO")) {
+				return "O";
+			}
+		}
+
+		// check if all the slots are full and if there is no winner
+		// return draw
+		for (int a = 0; a < 9; a++) {
+			if (Arrays.asList(board).contains(String.valueOf(a + 1))) {
+				break;
+			} else if (a == 8) {
+				return "draw";
+			}
+		}
 
 		// To enter the X Or O at the exact place on board.
-			System.out.println(	turn + "'s turn; enter a slot number to place "	+ turn + " in:");
-			return "None";
-		}
-		
+//			System.out.println(	turn + "'s turn; enter a slot number to place "	+ turn + " in:");
+		return "None";
+	}
 
-		/**
-		 * For printing the board
-		 */
-		public void printBoard()
-		{
-			System.out.println("|---|---|---|");
-			System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
-			System.out.println("|-----------|");
-			System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
-			System.out.println("|-----------|");
-			System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
-			System.out.println("|---|---|---|");
-		}
-		
-		/** 
-		 * Returns the String representation of TicTacToe object 
-		 */
-		 public String toString()
-		{
-				System.out.println("Winner: " + this.winner);
-				System.out.println("Current Turn: " + this.turn);
-			    this.printBoard();
-			    return "";
-		}
-		 
+	/**
+	 * For printing the board
+	 */
+	public void printBoard() {
+		System.out.println("|---|---|---|");
+		System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+		System.out.println("|---|---|---|");
+	}
+
+	/**
+	 * Returns the String representation of TicTacToe object
+	 */
+	public String toString() {
+		System.out.println("Winner: " + this.winner);
+		System.out.println("Current Turn: " + this.turn);
+		this.printBoard();
+		return "";
+	}
+
 }
